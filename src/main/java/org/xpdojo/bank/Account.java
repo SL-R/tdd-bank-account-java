9a0b2ded0db0ebf3786771094a6421fd9e374993
SL-R <office@rollwage.net>
2021-09-15 14:09:47 +0200

    draft

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,10 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    public Integer balance = 0;
+
+    public void deposit (int amount) {
+        balance += amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,15 +1,26 @@
 package org.xpdojo.bank;
 
+import static org.hamcrest.Matchers.is;
+//import static org.junit.;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
 
     @Test
-	@Disabled
+//	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        // assertThat("your first test").isBlank();
+
+        // arrange
+        Account account = new Account();
+
+        // act (add money
+        account.deposit(100);
+
+        //assert (check)
+        assertEquals(account.balance.intValue(), 100);
+
     }
 }
